b3b05d2110df6b779c66eb59289d1e881d807f61
Roderik Mogot <39650285+roderikmogot@users.noreply.github.com>
2021-12-17 13:41:52 +0800

    added app

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from helper import get_pred_label, load_model, load_img, convert
+
+model = load_model("big_dog_model.h5")
+
+st.markdown("""
+# Dog Breed Classifier
+""")
+
+image_file = st.file_uploader("Choose an image file", type=['jpeg', 'jpg', 'png'])
+
+if image_file:
+  st.image(load_img(image_file))
+
+  img = convert(image_file)
+
+  custom_preds = model.predict(img)
+  custom_pred_labels = [get_pred_label(custom_preds[i]) for i in range(len(custom_preds))]
+
+  st.write(f"Predicted: {custom_pred_labels[0]}, with an accuracy of {custom_preds[0].max()*100:.0f}%!")
