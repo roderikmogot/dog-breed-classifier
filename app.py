852ba56dd79533e798ed9e8d9399adf4e5b2596c
Roderik Mogot <roderikmogot17@gmail.com>
2022-01-01 12:36:46 +0800

    renamed title

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from helper import get_pred_label, load_model, load_img, convert, pred_stats, unique_breeds
 model = load_model("big_dog_model.h5")
 
-st.markdown("# Dog breed classifier")
+st.markdown("# Dog Breed Classifier")
 sd = st.sidebar.selectbox(
   "Types of dog that can be classified",
   unique_breeds
